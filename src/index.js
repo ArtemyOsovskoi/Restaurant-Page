047569c480b6839d09709f482adb61db9af040f9
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2024-04-12 11:19:47 +0300

    html text added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import _ from 'lodash';
+import Background from './background.jpg'
 
-console.log('hello');
+
+
+
+
+
+/*         <h1>Bookhouse Cafe</h1>
+        <h2>Serving freshly brewed coffee since 1918</h2>
+        <br>
+        <p>Bookhouse Cafe is a haven for book and coffee lovers alike, 
+        offering a cozy retreat where the aroma of freshly brewed coffee blends harmoniously with 
+        the allure of literary treasures. Indulge in the delightful fusion of caffeine and creativity at 
+        Bookhouse Cafe, where every sip and page invites you to savor moments of tranquility and inspiration. 
+        </p>
+        <br>
+        <p style="font-weight: bold;">Visit us at your leisure to experience the perfect blend of books and coffee.</p>
+        <footer style="color: ivory; text-align: center;">
+        <p>Copyright © 
+            <script>
+            document.write(new Date().getFullYear())
+            </script>
+        </p>
+        <a href="https://github.com/ArtemyOsovskoi">artemy.o</a>
+    </footer> */
