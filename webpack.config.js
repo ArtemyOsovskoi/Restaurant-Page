2edb43b0e5564d48526bc59eb9f10c5b57628feb
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2024-04-13 16:45:15 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [
+     rules: [
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-    ],
-  },
+     ],
+   },
 };
