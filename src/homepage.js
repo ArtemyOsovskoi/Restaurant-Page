f1ce8d0f92b7effc3a4cd76e28457a69beff4fe2
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2024-04-13 18:01:07 +0300

    homepage module

---
+++ src/homepage.js
@@ -0,0 +1,36 @@
+export function homepage() {
+    const content = document.querySelector('#content');
+
+    const header = document.createElement('h1');
+    header.innerText = 'Bookhouse Cafe';
+    header.setAttribute('class', 'header');
+
+    const headline = document.createElement('h2');
+    headline.innerText = 'Serving freshly brewed coffee since 1918';
+    headline.setAttribute('class', 'headline');
+
+    const description = document.createElement('p');
+    description.innerText = `Bookhouse Cafe is a haven for book and coffee lovers alike, 
+    offering a cozy retreat where the aroma of freshly brewed coffee blends harmoniously with 
+    the allure of literary treasures. Indulge in the delightful fusion of caffeine and creativity at 
+    Bookhouse Cafe, where every sip and page invites you to savor moments of tranquility and inspiration.`;
+    description.setAttribute('class', 'description');
+
+
+    const invitation = document.createElement('p');
+    invitation.innerText = 'Visit us at your leisure to experience the perfect blend of books and coffee.';
+    invitation.setAttribute('class', 'invitation');
+
+
+    const footer = document.createElement('footer');
+    footer.innerText = 'Copyright © artemy.o';
+    footer.setAttribute('class', 'footer');
+
+    content.appendChild(header, headline, description, invitation, footer);
+
+    return header, headline, description, invitation, footer
+}
+
+export function consoleText() {
+    console.log('It worked');
+}
