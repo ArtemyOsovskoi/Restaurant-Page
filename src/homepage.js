9fc93b9f7d22ab77ba75387e5281b5e2c01eb3c4
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2024-04-16 16:06:53 +0300

    Homepage html structure works from JS logic, DOM manipulation

--- src/homepage.js
+++ src/homepage.js
@@ -1,40 +1,47 @@
 export default function homepage() {
-    const content = document.getElementById('#content');
+    console.log('homepage func tesing');
 
+    const content = document.getElementById('content');
+
+    //Header
     const header = document.createElement('h1');
-    header.innerText = 'Bookhouse Cafe';
+    const headerNode = document.createTextNode("Bookhouse Cafe");
+    header.appendChild(headerNode);
+    content.appendChild(header);
     header.setAttribute('class', 'header');
 
+    //Headline
     const headline = document.createElement('h2');
-    headline.innerText = 'Serving freshly brewed coffee since 1918';
+    const headlineNode = document.createTextNode('Serving freshly brewed coffee and pastries since 1918');
+    headline.appendChild(headlineNode);
+    content.appendChild(headline);
     headline.setAttribute('class', 'headline');
 
+    //Description
     const description = document.createElement('p');
-    description.innerText = `Bookhouse Cafe is a haven for book and coffee lovers alike, 
+    const descriptionNode = document.createTextNode(
+    `Bookhouse Cafe is a haven for book and coffee lovers alike, 
     offering a cozy retreat where the aroma of freshly brewed coffee blends harmoniously with 
     the allure of literary treasures. Indulge in the delightful fusion of caffeine and creativity at 
-    Bookhouse Cafe, where every sip and page invites you to savor moments of tranquility and inspiration.`;
+    Bookhouse Cafe, where every sip and page invites you to savor moments of tranquility and inspiration.`
+    );
+    description.appendChild(descriptionNode);
+    content.appendChild(description);
     description.setAttribute('class', 'description');
 
-
+    //Invitation
     const invitation = document.createElement('p');
-    invitation.innerText = 'Visit us at your leisure to experience the perfect blend of books and coffee.';
+    const invitationNode = document.createTextNode(
+    'Visit us at your leisure to experience the perfect blend of books, pastries and coffee.'
+    );
+    invitation.appendChild(invitationNode);
+    content.appendChild(invitation);
     invitation.setAttribute('class', 'invitation');
 
-
+    //Footer
     const footer = document.createElement('footer');
-    footer.innerText = 'Copyright © artemy.o';
-    footer.setAttribute('class', 'footer');
-
-    content.appendChild(header);
-    content.appendChild(headline);
-    content.appendChild(description);
-    content.appendChild(invitation);
+    const footerNode = document.createTextNode('Copyright © artemy.o');
+    footer.appendChild(footerNode);
     content.appendChild(footer);
-
-    return header, headline, description, invitation, footer
+    footer.setAttribute('class', 'footer');
 }
-
-export function consoleText() {
-    console.log("Homepage Module Works!");
-};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import _ from 'lodash';
-import consoleText from './homepage.js';
 import homepage from './homepage.js';
 
 homepage();
-consoleText();
 
 console.log('index js is working as intended');
